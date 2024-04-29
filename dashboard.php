ae633d0060fa278ccf96ed3f97474365692d2d2b
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-04-28 23:53:24 -0400

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h2>Welcome, <?php echo $_SESSION['username']; ?></h2>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
