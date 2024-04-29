023b729b49dc6a350d1b030157784cf2d2e04e7d
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-04-29 01:22:50 -0400

    Update config.php

--- config.php
+++ config.php
@@ -10,5 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
 }
 ?>
