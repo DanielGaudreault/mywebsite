3a113d0f61eb7a031d447e583ee45b7b6ac23fb3
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-04-28 23:51:17 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "your_database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
