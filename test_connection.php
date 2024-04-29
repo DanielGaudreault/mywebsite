ae59351e2a888f18415fd5842455ed2973bca74d
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-04-29 02:33:09 -0400

    Create test_connection.php

---
+++ test_connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+$conn->close();
+?>
