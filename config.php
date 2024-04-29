5b8922e76b8c4593a86fcd40678f09d7935f9968
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-04-29 00:53:02 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // Replace with your MySQL server hostname
-$username = "your_username"; // Replace with your MySQL username
-$password = "your_password"; // Replace with your MySQL password
-$dbname = "your_database_name"; // Replace with your MySQL database name
+$servername = "localhost";
+$username = "root"; // Your MySQL username
+$password = ""; // Your MySQL password
+$dbname = "mywebsite"; // Your MySQL database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
